3100a2a92d83167d4a6306ba4a3d961841d09969
Leandro Almeida <leo.leandro.ars@gmail.com>
2024-08-25 17:46:38 -0300

    feat: adds design to more and algorithms pages

--- echoeasy-app/app/(tabs)/algorithms.tsx
+++ echoeasy-app/app/(tabs)/algorithms.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList, Image } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+import ItemCard from '../../components/ItemCard';
+import SearchInput from '../../components/SearchInput';
+
+type Item = {
+  title: string
+  description: string
+  image: string
+};
+const data: Item[] = [{ title: "Disfunção Diastólica Incluindo o Strain", description: "", image: "" },];
 
 const Algorithms: React.FC = () => {
-  
+
   return (
-    <SafeAreaView className="bg-[#F6F6F6] h-full">
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full h-full flex justify-center items-center p-6">
+    <SafeAreaView className="bg-[#F6F6F6] h-full p-6">
+      <Text className='font-interMedium text-2xl'>Algoritmos</Text>
+      <View className="w-full h-full flex items-center">
 
+        <SearchInput
+          placeholder='Pesquise por um algoritmo'
+          icon="search-outline"
+        />
 
-          <Text>Algoritmos</Text>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <ItemCard
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          )}
+        />
+      </View>
 
-
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 };
