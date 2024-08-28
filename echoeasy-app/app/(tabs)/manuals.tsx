e3c0cf6917000606f87089a093f976cdd401b2c4
Leandro Almeida <leo.leandro.ars@gmail.com>
2024-08-28 11:12:11 -0300

    feat: integrations of the docs to manuals page

--- echoeasy-app/app/(tabs)/manuals.tsx
+++ echoeasy-app/app/(tabs)/manuals.tsx
@@ -1,30 +1,57 @@
-import React from 'react';
-import { View, Text, ScrollView, FlatList, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import ItemCard from '../../components/ItemCard';
 import SearchInput from '../../components/SearchInput';
+import { DocService } from '../../src/service/DocService';
+import { useGlobalContext } from '../../src/context/GlobalProvider';
 
 type Item = {
-  title: string
-  description: string
-  image: string
+  title: string;
+  description: string;
+  image: string;
 };
-const data: Item[] = [{ title: "Título", description: "Descrição", image: "" }, { title: "Interpretação de Imagens", description: "2 tópicos", image: "https://ufmg.br/thumbor/ycQy_k9_9Us1_NMirT293_HVnQw=/0x0:734x491/712x474/https://ufmg.br/storage/b/5/1/8/b518ab2347d168580bd6d18c12f47b7c_15354817310865_1687263323.png" },];
 
 const Manuals: React.FC = () => {
+  const { token } = useGlobalContext();
+  const [docs, setDocs] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchDocuments = async () => {
+    setLoading(true); //provisorio
+    try {
+      const docService = new DocService();
+      const response = await docService.getAllDocuments(token);
+      setDocs(response.data as Item[]);
+    } catch (error: any) {
+      console.error('Error fetching documents:', error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+
+    fetchDocuments();
+
+  }, []);
+
+  if (loading) return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text>Loading...</Text>
+    </SafeAreaView>
+  );
 
   return (
     <SafeAreaView className="bg-[#F6F6F6] h-full p-6">
       <Text className='font-interMedium text-2xl'>Manuais</Text>
       <View className="w-full h-full flex items-center">
-
         <SearchInput
           placeholder='Pesquise por um manual'
           icon="search-outline"
         />
-
         <FlatList
-          data={data}
+          data={docs}
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <ItemCard
@@ -35,7 +62,6 @@ const Manuals: React.FC = () => {
           )}
         />
       </View>
-
     </SafeAreaView>
   );
 };
