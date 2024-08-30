65254b9309b7e1a8b2c57344364ce5c09b56c086
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-08-30 12:14:15 -0300

    feat: increase y padding to algorithms area

--- echoeasy-app/app/(tabs)/algorithms.tsx
+++ echoeasy-app/app/(tabs)/algorithms.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import { View, Text, ScrollView, FlatList, Image } from 'react-native';
+import React from "react";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import ItemCard from '../../components/ItemCard';
-import SearchInput from '../../components/SearchInput';
+import ItemCard from "../../components/ItemCard";
+import SearchInput from "../../components/SearchInput";
 
 type Item = {
-  title: string
-  description: string
-  image: string
+  title: string;
+  description: string;
+  image: string;
 };
-const data: Item[] = [{ title: "Disfunção Diastólica Incluindo o Strain", description: "", image: "" },];
+const data: Item[] = [
+  {
+    title: "Disfunção Diastólica Incluindo o Strain",
+    description: "",
+    image: "",
+  },
+];
 
 const Algorithms: React.FC = () => {
-
   return (
-    <SafeAreaView className="bg-[#F6F6F6] h-full p-6">
-      <Text className='font-interMedium text-2xl'>Algoritmos</Text>
+    <SafeAreaView className="bg-[#F6F6F6] h-full p-6 py-10">
+      <Text className="font-interMedium text-2xl">Algoritmos</Text>
       <View className="w-full h-full flex items-center">
-
         <SearchInput
-          placeholder='Pesquise por um algoritmo'
+          placeholder="Pesquise por um algoritmo"
           icon="search-outline"
         />
 
@@ -35,7 +39,6 @@ const Algorithms: React.FC = () => {
           )}
         />
       </View>
-
     </SafeAreaView>
   );
 };
