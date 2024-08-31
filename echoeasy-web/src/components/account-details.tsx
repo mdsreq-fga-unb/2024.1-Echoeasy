e0a198dba71bc6d530cffcc630b12de44eaf9218
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-08-31 16:52:48 -0300

    feat: Add image upload functionality to AccountDetails component

--- echoeasy-web/src/components/account-details.tsx
+++ echoeasy-web/src/components/account-details.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTokenContext } from "@/contexts/TokenContext";
 import { api } from "@/services/api";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
@@ -14,109 +15,135 @@ import {
 } from "./ui/card";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { toast } from "./ui/use-toast";
 
 export default function AccountDetails() {
-  const { user } = useTokenContext();
+  const { user, setUser } = useTokenContext();
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const [isEditing, setIsEditing] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
 
   const getUserAvatarLetters = () => {
     if (user?.name && user?.lastname) {
       return `${user.name[0]}${user.lastname[0]}`.toUpperCase();
     }
-    return "U"; // Default placeholder if user name or lastname is not available
+
+    return "N/A";
   };
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files && files.length > 0) {
       const file = files[0];
-      setSelectedFile(file); // Armazene o objeto File real
-      setPreviewImage(URL.createObjectURL(file)); // Para exibição de pré-visualização
+      setSelectedFile(file);
+      setPreviewImage(URL.createObjectURL(file));
       setIsEditing(true);
     }
   };
 
   const handleSave = async () => {
     if (!selectedFile) return;
+    setIsUploading(true);
     const formData = new FormData();
-    formData.append("image", selectedFile); // Use o objeto File aqui
+    formData.append("image", selectedFile);
 
     try {
-      await api.post(`/usuarios/update_photo?_id=${user?._id}`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const userResponse = await api.post(
+        `/usuarios/update_photo?_id=${user?._id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
       setIsEditing(false);
-      setPreviewImage(null); // Limpa a pré-visualização após salvar
+      setPreviewImage(null);
+      setUser(userResponse.data);
+      toast({
+        title: "Foto atualizada com sucesso!",
+        description: "Sua foto de perfil foi atualizada com sucesso.",
+      });
     } catch (error) {
       console.error("Erro ao atualizar a foto:", error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  const renderImageOrAvatar = () => {
+    if (previewImage || user?.image) {
+      return (
+        <Image
+          src={(previewImage || user?.image) ?? ""}
+          alt="Avatar"
+          className="rounded-sm self-center"
+          width={200}
+          height={200}
+        />
+      );
+    } else {
+      return (
+        <Avatar className="h-[200px] w-[200px] self-center">
+          <AvatarImage src="#" alt="Avatar" />
+          <AvatarFallback className="text-4xl">
+            {getUserAvatarLetters()}
+          </AvatarFallback>
+        </Avatar>
+      );
+    }
+  };
+
+  const renderActionButton = () => {
+    if (isEditing) {
+      return (
+        <>
+          {isUploading ? (
+            <Button disabled className="w-fit self-center text-xs p-2 h-fit">
+              <Loader2 className="w-4 h-fit animate-spin" />
+              Salvando...
+            </Button>
+          ) : (
+            <Button
+              className="w-fit self-center text-xs p-2 h-fit"
+              onClick={handleSave}
+            >
+              Salvar
+            </Button>
+          )}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Button className="w-fit self-center text-xs p-2 h-fit">
+            <label htmlFor="upload-photo" className="cursor-pointer">
+              Alterar foto
+            </label>
+          </Button>
+          <input
+            type="file"
+            id="upload-photo"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="hidden"
+          />
+        </>
+      );
     }
   };
 
   return (
     <Card className="w-full h-fit">
       <CardHeader>
-        <CardTitle>Sua Conta </CardTitle>
+        <CardTitle>Sua Conta</CardTitle>
         <CardDescription>Informações sobre a sua conta.</CardDescription>
-        {user?.image || previewImage ? (
-          <div className="flex flex-col gap-4">
-            <Image
-              src={(previewImage || user?.image) ?? ""}
-              alt="Avatar"
-              className="rounded-sm self-center"
-              width={200}
-              height={200}
-            />
-            {isEditing ? (
-              <Button
-                className="w-fit self-center text-xs p-2 h-fit"
-                onClick={handleSave}
-              >
-                Salvar
-              </Button>
-            ) : (
-              <>
-                <Button className="w-fit self-center text-xs p-2 h-fit">
-                  <label htmlFor="upload-photo" className="cursor-pointer">
-                    Alterar foto
-                  </label>
-                </Button>
-                <input
-                  type="file"
-                  id="upload-photo"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  className="hidden"
-                />
-              </>
-            )}
-          </div>
-        ) : (
-          <div className="flex flex-col gap-4">
-            <Avatar className="h-[200px] w-[200px] self-center">
-              <AvatarImage src="#" alt="Avatar" />
-              <AvatarFallback className="text-4xl">
-                {getUserAvatarLetters()}
-              </AvatarFallback>
-            </Avatar>
-            <Button className="w-fit self-center text-xs p-2 h-fit">
-              <label htmlFor="upload-photo" className="cursor-pointer">
-                Alterar foto
-              </label>
-            </Button>
-            <input
-              type="file"
-              id="upload-photo"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="hidden"
-            />
-          </div>
-        )}
+        <div className="flex flex-col gap-4">
+          {renderImageOrAvatar()}
+          {renderActionButton()}
+        </div>
       </CardHeader>
       <CardContent className="space-y-2">
         <Label htmlFor="email">Email</Label>
